849a51bf392aa608406b0cf4f261d903ac61aebc
Yann <yann.sady.pro@gmail.com>
2024-02-12 12:10:28 +0100

    Create myTodoLocalActivity

---
+++ app/src/main/java/com/example/mytodolist/View/MyTodoLocalActivity.java
@@ -0,0 +1,16 @@
+package com.example.mytodolist.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mytodolist.R;
+
+public class MyTodoLocalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_todo_local);
+    }
+}
